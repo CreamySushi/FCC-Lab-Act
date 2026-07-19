62d0bc42a0fb96fc80359bada5f838e94e392428
CreamySushi <johnaaronflavien@gmail.com>
2026-07-19 15:59:58 +0800

    replace the text with '...' based on the number input and shorten the text

---
+++ Functions/Implement the Truncate a String Algorithm.js
@@ -0,0 +1,25 @@
+/*
+Implement the Truncate a String Algorithm
+In this lab, you will practice truncating a string to a certain length.
+
+Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+You should have a function truncateString that accepts two arguments, the first one a string, the second one a number.
+If the length of the string is more than the given number, the string should be truncated to reduce the length so that it is equal the given number, and ... should be appended at the end of the truncated string.
+If the length of the string is equal to or lower than the given number, the string should be returned unchanged.
+*/
+
+function truncateString (string, num){
+  const truncate = string.slice(num)
+  let replaced;
+  if (string.length <= num){
+     replaced = string
+  }else if (string.length == num) {
+      replaced = string
+  }else{
+    replaced = string.replace(truncate, "...")
+  }
+  return replaced
+}
